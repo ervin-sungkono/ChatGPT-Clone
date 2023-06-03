9299d11fe33bf43f733200db70bbdbde937140b2
ervin-sungkono <ervinsung@gmail.com>
2023-06-03 14:37:28 +0700

    refactor styling

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -11,22 +11,27 @@ export default function Sidebar(){
         initDropdowns()
     })
     return(
-        <section className="relative w-[260px] h-screen flex flex-col gap-2 dark:bg-gray-900 p-2">
+        <section className="relative w-[260px] h-screen flex flex-col gap-2 dark:bg-gray-900 p-2 group">
             <button className="w-full flex items-center gap-2 p-3 border dark:border-white/20 rounded-lg dark:hover:bg-white/5 transition-colors duration-300">
                 <AiOutlinePlus size={16}/>
                 <p className="text-sm font-medium">New Chat</p>
             </button>
-            <div className="flex flex-col flex-grow overflow-hidden hover:overflow-y-scroll dark:bg-white/5 border-b dark:border-b-white/20">
+            <div className="flex flex-col flex-grow overflow-hidden group-hover:overflow-y-scroll border-b dark:border-b-white/20">
 
             </div> 
-            <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="flex justify-between items-center dark:hover:bg-gray-800 dark:text-white rounded-lg p-3 transition-colors duration-300" type="button">
+            <button 
+                id="profileDropdownButton"
+                data-dropdown-toggle="profileDropdown"
+                className="flex justify-between items-center dark:hover:bg-gray-800 dark:text-white rounded-lg p-3 transition-colors duration-300" 
+                type="button"
+            >
                 <div className="flex items-center gap-2">
                     <Image src={"/placeholder_user.jpg"} width={20} height={20} alt="" className="rounded object-cover"/>
                     <p className="text-sm font-medium">Unknown User</p>
                 </div>
                 <BsThreeDots size={16} className="text-gray-500"/>
             </button>
-            <div id="dropdown" className="w-[244px] z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-black">
+            <div id="profileDropdown" className="w-[244px] z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-black">
                 <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
                     <li>
                         <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-import colors from 'tailwindcss/colors'
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
@@ -9,18 +7,15 @@ module.exports = {
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-    },
     extend: {
       colors: {
         gray: {
           50: '#F7F7F8',
           100: '#ECECF1',
+          300: '#C5C5D2',
+          400: '#ACACBE',
           500: '#8E8EA0',
+          700: '#40414F',
           800: '#343541',
           900: '#202123',
         },
