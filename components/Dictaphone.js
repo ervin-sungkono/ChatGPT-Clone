b70c7c5baba6c353239a7de50373a42e2dba5205
ervin-sungkono <ervinsung@gmail.com>
2023-07-08 17:18:03 +0700

    adjust microphone disabled styling

--- components/Dictaphone.js
+++ components/Dictaphone.js
@@ -39,7 +39,7 @@ export default function Dictaphone({ setPreviewSpeech, handleSpeechEnd }){
     <button 
         type='button' 
         disabled={!isMicrophoneAvailable} id='mic-btn'
-        className={`${listening ? "bg-green text-white" : "bg-white dark:bg-gray-700 text-gray-600 dark:text-gray-400 hover:bg-gray-300 dark:hover:bg-gray-900 border-black/10 dark:border-white/10"} disabled:text-gray-600/40 dark:disabled:text-gray-400/40 border p-3 shadow-md rounded-full transition-colors duration-200`}
+        className={`${(listening && isMicrophoneAvailable) ? "bg-green text-white" : "bg-white dark:bg-gray-700 text-gray-600 dark:text-gray-400 hover:bg-gray-300 dark:hover:bg-gray-900 border-black/10 dark:border-white/10 disabled:bg-gray-200 dark:disabled:bg-gray-800"} disabled:text-gray-600/40 dark:disabled:text-gray-400/40 border p-3 shadow-md rounded-full transition-colors duration-200`}
         onTouchStart={startListening}
         onMouseDown={startListening}
         onTouchEnd={SpeechRecognition.stopListening}
